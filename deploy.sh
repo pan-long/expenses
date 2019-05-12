96b8831ac59a3618e5777c3797886702b7a5b715
Pan Long <aga.panlong@gmail.com>
2019-05-12 23:00:13 +0800

    Change deploy.sh so to deploy to gh-pages.

--- deploy.sh
+++ deploy.sh
@@ -1,30 +1,10 @@
 #!/bin/sh
 
-if [ $# -eq 0 ]
-  then
-    echo "Add a commit message"
-    return 1
-fi
-
-git add .
-git commit -m "$1"
-git push origin master
-
-# expenses.chodounsky.net
-REACT_APP_SHEET_ID=18uwYwUAVw0H5bhszMgAORmvAN2APxAtJI3FB-XH7Dzk npm run build
-cp -a build/. production/
-cd production
-git add .
-git commit -m "$1"
-git push origin production
-cd ..
-
-# github pages
-
+npm install
 npm run build
-cp -a build/. demo/
-cd demo
+cp -a build/. app/
+cd app
 git add .
-git commit -m "$1"
+git commit -m "Automatic build from HEAD."
 git push origin gh-pages
 cd ..
