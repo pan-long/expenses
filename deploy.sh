0069a9c867503d3f5a0cbf3734fee20b19c2473c
Pan Long <aga.panlong@gmail.com>
2019-05-12 23:14:32 +0800

    Checkout to gh-pages before commiting.

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
+git checkout gh-pages
 npm install
 npm run build
 cp -a build/. app/
 cd app
 git add .
 git commit -m "Automatic build from HEAD."
-git push origin gh-pages
+git push origin
 cd ..
